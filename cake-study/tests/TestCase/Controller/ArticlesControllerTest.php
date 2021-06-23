011d61f2cd3603fe3a9474252da73684d5a3797d
LeBinhAn <lvba2102@gmail.com>
2021-06-23 11:53:46 +0700

    Init project

---
+++ cake-study/tests/TestCase/Controller/ArticlesControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\ArticlesController;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Controller\ArticlesController Test Case
+ *
+ * @uses \App\Controller\ArticlesController
+ */
+class ArticlesControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Test initial setup
+     *
+     * @return void
+     */
+    public function testInitialization()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
